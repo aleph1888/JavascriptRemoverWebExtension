c69572c67583fff69e99b0570aeef2d6d88f6bae
aleph <ox@ismy.band>
2016-06-21 19:53:24 +0200

    Added input controls, events and style

--- change.org-inpage-toolbar-ui/loadbar/ui.js
+++ change.org-inpage-toolbar-ui/loadbar/ui.js
@@ -6,23 +6,3 @@ document.querySelector("#toggle").addEventListener("click", function() {
   // Ask the background page to toggle the toolbar on the current tab
   port.postMessage("toggle-in-page-toolbar");
 });
-
-// Handle click events on the toolbar button.
-document.querySelector("#buttonLoad").addEventListener("click", function() {
-  // Ask the background page to toggle the toolbar on the current tab
-  	var payload = {
-          signerData: getSignerFormData(), 
-          petitionUrl: getPetitionUrl() 
-      };
-	port.postMessage("button-load-click", payload);
-});
-
-// Handle click events on the toolbar button.
-document.querySelector("#buttonSign").addEventListener("click", function() {
-  // Ask the background page to toggle the toolbar on the current tab
-    var payload = {
-          signerData: getSignerFormData(), 
-          petitionId: getPetitionId() 
-    };
-	port.postMessage("button-sign-click", payload);
-});
